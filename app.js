4ebdac756054c6e6ffffc2d16eb07702fc895ba3
julianalyvaiko <50122718+julianalyvaiko@users.noreply.github.com>
2019-09-21 15:51:07 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,86 +1,53 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-// select the element to put the body of the table in
-
 var tbody = d3.select("tbody");
 
-// select the filter button for future use
+// Automatic Table and Date Search
+// Refactor to use Arrow Functions
+function loadTableData(InputData) {
+    InputData.forEach((UFOreport) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOreport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  }); 
+}
+loadTableData(tableData);
 
-var submit = d3.select("#filter-btn");
+//---Listen for events and search through the `date/time` column to find rows that match input
+var button = d3.select("#filter-btn");
+var Date = d3.select("#datetime");
+var City = d3.select("#city");
+var State = d3.select("#state");
+var Country = d3.select("#country");
+var Shape = d3.select("#shape");
 
-// create function to display table
-function displayTable(info) {
- info.forEach((event) => {
-   var tr = tbody.append("tr");
-     tr.append("td").text(event.datetime);
-     tr.append("td").text(event.city);
-     tr.append("td").text(event.state);
-     tr.append("td").text(event.country);
-     tr.append("td").text(event.shape);
-     tr.append("td").text(event.durationMinutes);
-     tr.append("td").text(event.comments);
-   });
-};
+// Clicks the button to filter data
+button.on("click", function() {
+    d3.event.preventDefault(); // Prevent the page from refreshing
+  
+    var inputDate  = Date.property("value"); // Get the value property of the input element
+    var inputCity  = City.property("value"); 
+    var inputState = State.property("value"); 
+    var inputCountry = Country.property("value"); 
+    var inputShape = Shape.property("value"); 
+    console.log(inputDate)
 
-// display initial table
-displayTable(tableData);
-
-// create function that filters based on the entry and which type
-   
-function filtering(selection, entry) {
-
-filteredData = tableData.filter(event => event[selection] === entry);
-
-displayTable(filteredData);
-
-};
-
-// When user clicks button do this
-
-submit.on("click", function() {
-
-// prevent reloading of the screen
-
- d3.event.preventDefault();
-
-// remove previous rows from display
-
- tbody.html('');
-
-// Select the input element and get the raw HTML node and value
-
-    var dateInput = d3.select("#datetime").property("value");
-    var cityInput = d3.select('#city').property("value");
-    var stateInput = d3.select("#state").property("value");
-    var countryInput = d3.select("#country").property("value");
-    var shapeInput = d3.select("#shape").property("value");
-
-    // if input is specific type and variable display for those types
-
-    if (dateInput) {
-
-        filtering('datetime', dateInput);
-
-    } else if (cityInput) {
-
-        filtering('city', cityInput);
-
-    } else if (stateInput) {
-
-        filtering('state', stateInput);
-
-    } else if (countryInput) {
-
-        filtering('country', countryInput);
-
-    } else if (shapeInput) {
-
-        filtering('shape', shapeInput);
-
-    } else {
-
-        alert('You didn\'t input anything')
-    };
-});
+    // Create Filtered dataset based on Input Values
+    if (inputDate) {
+        var filterdata = tableData.filter(onerec => onerec.datetime === inputDate);}
+    if (inputCity) {
+        var filterdata = filterdata.filter(onerec => onerec.city === inputCity);}
+    if (inputState) {
+        var filterdata = filterdata.filter(onerec => onerec.state === inputShape);}
+    if (inputCountry) {
+        var filterdata = filterdata.filter(onerec => onerec.country === inputCountry);}
+    if (inputShape) {
+        var filterdata = filterdata.filter(onerec => onerec.shape === inputShape);}
+        
+    // Build new UFO Table with the filtered subset of UFO Sighting data
+    tbody.html("");
+    loadTableData(filterdata);
+  });
